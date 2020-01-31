8b8011cfecb181a6126f832a5df832d9adf5dc70
Rajeswari Tedla <rajeswari_tedla@ad.infosys.com>
2020-01-31 15:19:13 +0530

    aws

---
+++ src/main/java/com/example/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo") 
+public class DemoController {
+	
+	@GetMapping("/getRes")
+	public String getEmployeeByID() {
+		return "You are successful in contacting me";
+	}
+	
+
+}
